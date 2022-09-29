52087200f9a1d982a5f64a92e6ff41ba0f988cf0
Luis Felipe Porto <luis.porto@seap.ma.gov.br>
2022-09-29 16:19:03 -0300

    [feature]-atualizando responde de lead

--- src/main/java/com/microsservices/api/marketing/response/LeadResponse.java
+++ src/main/java/com/microsservices/api/marketing/response/LeadResponse.java
@@ -2,12 +2,15 @@ package com.microsservices.api.marketing.response;
 
 import com.microsservices.api.marketing.application.LeadSevice;
 import com.microsservices.api.marketing.domain.Lead;
-import com.microsservices.api.marketing.infra.config.utils.RabbitMQConstantes;
 import com.microsservices.api.marketing.infra.service.RabbitmqService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("v1/marketing")
@@ -25,5 +28,13 @@ public record LeadResponse(LeadSevice leadSevice, RabbitmqService rabbitmqServic
     @GetMapping("/buscarPorEmail")
     public Lead getLeadPorEmail(@PathVariable String email){
         return leadSevice.buscarPorEmail(email);
+    }
+    @PatchMapping("/convertLead")
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String,String> updateLead(@RequestBody String email){
+        leadSevice.converterLead(email);
+        HashMap<String, String> mapString = new HashMap<>();
+        mapString.put("email",email);
+        return mapString;
     }
 }
